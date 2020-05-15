e702d7903490d89f10c4e1c68a41704e64a4bc3f
xluo62 <xluo62@uwo.ca>
2020-05-15 16:45:07 -0400

    Sign in With Google

--- src/App.js
+++ src/App.js
@@ -5,17 +5,40 @@ import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shoppage/shop.component';
 import Header from './components/header/header.component';
 import SignINAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
-function App() {
-  return (
-    <div>
-      <Header/>
-      <Switch>
-      <Route exact path='/' component={HomePage}></Route>
-      <Route exact path='/shop/hats' component={ShopPage}></Route>
-      <Route exact path='/signin' component={SignINAndSignUpPage}></Route>
-      </Switch>
-    </div>
-  );
+import { auth } from './firebase/firebase.utils';
+
+class App extends React.Component {
+  constructor(props){
+    super(props);
+
+    this.state={
+      currentUser: null
+    }
+  }
+  unsubscribeFromAuth = null;
+  componentDidMount() {
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
+      this.setState({ currentUser: user});
+      console.log(user);
+    })
+  }
+  componentWillUnmount() {
+    this.unsubscribeFromAuth();
+  }
+  component
+  render() {
+    return (
+      <div>
+        <Header/>
+        <Switch>
+        <Route exact path='/' component={HomePage}></Route>
+        <Route exact path='/shop/hats' component={ShopPage}></Route>
+        <Route exact path='/signin' component={SignINAndSignUpPage}></Route>
+        </Switch>
+      </div>
+    );
+  }
+ 
 }
 
 
